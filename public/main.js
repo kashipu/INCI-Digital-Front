408d42124dc916cba3ee11cee21f29cabab5c944
kashipu <graficowm@gmail.com>
2020-04-16 23:45:44 -0500

    0.3
    Accesibilidad con Javascript en el menú

--- public/main.js
+++ public/main.js
@@ -1,8 +1,27 @@
-const btnMenu = document.querySelector('.btn-menu');
-
 function toggle() {
-    var menuShow = document.getElementById('menu-toggle');
-    var bodyShow = document.getElementById('container');
-    menuShow.classList.toggle('show');
-    bodyShow.classList.toggle('show');
+    let menuShow = document.getElementById('menu-toggle');
+
+    menuShow.classList.toggle('no-show');
+    console.log(menuShow)
+    if (menuShow.classList.contains("no-show")) {
+        console.log("Menú Oculto");
+        let mainHome = document.getElementById('home');
+        let burguerSet = document.getElementById('burguer-menu');
+        mainHome.style.display = 'block';
+        mainHome.setAttribute("aria-hidden", "false");
+        burguerSet.setAttribute("aria-expanded", "false")
+    } else {
+        console.log("Menu Mostrandose");
+        let mainHome = document.getElementById('home');
+        let burguerSet = document.getElementById('burguer-menu');
+        mainHome.style.display = 'none';
+        mainHome.setAttribute("aria-hidden", "true");
+        burguerSet.setAttribute("aria-expanded", "true")
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.getElementById('botonAtras').focus();
 }
